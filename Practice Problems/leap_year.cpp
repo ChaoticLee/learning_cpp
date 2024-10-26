196ffa7484584eb0140ec33ed83800ad924b1794
Hailie Nash <91491646+ChaoticLee@users.noreply.github.com>
2024-10-26 12:03:52 -0700

    Update leap_year.cpp
    adding commentary on the program's use

--- Practice Problems/leap_year.cpp
+++ Practice Problems/leap_year.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std; 
 
+/*This is a practice of making a conditional loop to see if a given year is a leap year. It wants a year that is larger than 1000, and then checks to see if the year is 
+modulo 4, and 100, or can be modulo 400, to be consider a leap year. If not, it will continue to prompt the user to enter a year until a leap year is found.*/
 int main() {
   
   int year; 
