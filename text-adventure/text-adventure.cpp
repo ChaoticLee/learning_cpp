103ec9cf1f5f57aeb671775f9cbf8d7a8dd487e5
Hailie Nash <91491646+ChaoticLee@users.noreply.github.com>
2024-12-23 14:25:32 -0800

    Create text-adventure.cpp
    codeacademy c++ project to learn case/if-else statement switching.

---
+++ text-adventure/text-adventure.cpp
@@ -0,0 +1,25 @@
+/*This project is to be a text adventure for the player to go through some type o storyline. Your decisions make decisions that cause  achange in the result. Think Dating Visual Novel.*/ 
+
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std; 
+int user; 
+int main(){
+    cout <<"============================\n     What's your deal?     \n============================\n \n 1 - Overview\n 2 - New Game\n 3 - Exit Game ";
+    cin >> user; 
+    switch(user)
+    {
+  case 1: 
+  {cout << "You're a contestant on the game show, name that price, if you are unable to, you lose! you and your fellow contestants are trying your best to make it out to the grand prize...and with your life!!! ";}
+// Start of game
+  case 2: {cout<<"W"}
+
+  case 3: {exit(0);}
+  };
+  //end of switch
+
+  //end of main fc
+  };
+
+}
